c5c3f99f223587a987f34efb21224e127604e07a
Zeerak Kafeel <80202081+IamZeek@users.noreply.github.com>
2023-09-30 23:12:56 +0500

    Commiting angular chatapp fronten

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TokenInterceptor } from './interceptors/token.interceptor';
+import { UserdataComponent } from './components/userdata/userdata.component';
+import { ChatpageComponent } from './components/chatpage/chatpage.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ChatDataComponent } from './components/chat-data/chat-data.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { PrivateChatComponent } from './components/private-chat/private-chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    UserdataComponent,
+    ChatpageComponent,
+    NavbarComponent,
+    ChatDataComponent,
+    MessagesComponent,
+    PrivateChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
